09e05a6d2b602c95477108b406c9a609e7e336d8
Paul Janota <pwjanota1@buffs.wtamu.edu>
2026-08-31 20:48:24 -0500

    Display the list on the home page

--- pages/views.py
+++ pages/views.py
@@ -3,10 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    items = [
+        {'name': 'Computer Hardware Repair', 'completed': True},
+        {'name': 'Software Skills', 'completed': True},
+        {'name': 'Basic Coding Skills', 'completed': True},
+        {'name': 'Advanced Coding Skills', 'completed': False},
+        {'name': 'Complete CIDM 3312', 'completed': False},
+    ]
+    context = {'items': items}
+    return render(request, 'pages/home.html', context)
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'pages/about.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'pages/contact.html')
